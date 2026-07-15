ade65ca00a1aebf5dba8a44e90cf2d3586c779c5
Pankaj Singh <pankajsingh1397@gmail.com>
2026-07-15 12:10:00 +0530

    refactor: update tooltip

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,8 +3,8 @@ import { GoInfo } from 'react-icons/go'
 function Tooltip({tip} : {tip : string}) {
     return (
         <div className="group relative">
-            <GoInfo className="w-3 h-3 text-slate-400" />
-            <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 px-3 py-2 border shadow-sm border-slate-200 bg-slate-50 text-slate-600 text-xs rounded-md opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-50">
+            <GoInfo className="w-3 h-3 text-slate-400 dark:text-slate-500" />
+            <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 px-3 py-2 border shadow-sm border-slate-200 dark:border-slate-600 bg-slate-50 dark:bg-slate-800 text-slate-600 dark:text-slate-300 text-xs normal-case rounded-md invisible opacity-0 group-hover:visible group-hover:opacity-100 transition-opacity whitespace-nowrap z-50 pointer-events-none">
                 {tip}
             </div>
         </div>
